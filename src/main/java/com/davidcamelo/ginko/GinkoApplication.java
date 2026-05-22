fc66365b5f3d29544205ecb1f905539f5c7828c6
David Camelo <dacaro16@gmail.com>
2026-05-22 17:58:58 -0500

    Initial Spring Boot project setup

---
+++ src/main/java/com/davidcamelo/ginko/GinkoApplication.java
@@ -0,0 +1,13 @@
+package com.davidcamelo.ginko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GinkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GinkoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/davidcamelo/ginko/GinkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidcamelo.ginko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GinkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
